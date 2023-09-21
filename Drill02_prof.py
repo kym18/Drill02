809e0ffb32888646c71f7170f2674743f8661d70
kym18 <cooiyoo@naver.com>
2023-09-21 12:20:22 +0900

    그리기함수추가
    render_frame

--- Drill02_prof.py
+++ Drill02_prof.py
@@ -5,36 +5,32 @@ open_canvas()
 character = load_image('character.png')
 grass = load_image('grass.png')
 
-clear_canvas_now()
-grass.draw_now(400, 30)
-character.draw_now(400, 90)
-delay(1)
+
+def render_frame(x, y):
+    clear_canvas_now()
+    grass.draw_now(400, 30)
+    character.draw_now(x, y)
+    delay(0.1)
 
 def run_circle():
     print('CIRCLE')
     cx, cy = 400, 300
-
     r = 200
-    #0도부터 360도까지 5도TLr
-    for deg in range(0, 360, 5):
+    
+    for deg in range(0, 360, 5):   #0도부터 360도까지 5도씩
         x = r * math.cos(math.radians(deg)) + cx
         y = r * math.sin(math.radians(deg)) + cy   
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, y)
-        delay(0.01)
+        render_frame(x, y)
             
     #pass :아무것도 안한다. 근데 실행 ㄱㄴ 아무것도 하지않는 함수의 기능 함.(뼈대 만들기)
+
 
 def run_rectangle():
     print('RECTANGLE')
 
     #bottom line
     for x in range(20, 780+1, 5):
-        clear_canvas_now()
-        grass.draw_now(400, 30)
-        character.draw_now(x, 90)
-        delay(0.01)
+        render_frame(x, 90) #x,y 위치에 캐릭터 그려줄 수 있는 함수
     
 
 
